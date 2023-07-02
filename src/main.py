6078e240db73f0790a3a18aab61627e65e3cc681
Roman Marakov <marakofr76@yandex.ru>
2023-07-02 11:58:26 +0300

    Выполнение курсовой работы: Доработка кода, сокращение

--- src/main.py
+++ src/main.py
@@ -1,65 +1,66 @@
 from src.api_platforms import HeadHunterAPI, SuperJobAPI
-from src.selection_of_vacancies import HH_user_vacancies, open_file_vacancies_hh, SJ_user_vacancies, Output_user, \
-    open_file_vacancies_sj
+from src.selection_of_vacancies import User_vacancies, Output_user, recording, open_file_vacancies
 
+""" Работа с пользователем """
 user_vacancy = (input('Какая вакансия Вас интересует?\n'))
+
+api = [HeadHunterAPI(user_vacancy), SuperJobAPI(user_vacancy)]
+user_vac = User_vacancies(api[0], api[1])
+user_vac.save_user_vacancy()
+recording(user_vac.vacancies)
+
 platforms_input = (input('На какой площадке вы хотите осуществить поиск? HH (HeadHunter), SJ (SuperJob)\n').lower())
 if platforms_input == 'HH'.lower():
-    api = HeadHunterAPI(user_vacancy)
-    obj_user_vac = HH_user_vacancies(api)
-    obj_user_vac.save_user_vacancy()
-    if not open_file_vacancies_hh():
+    if not open_file_vacancies():
         print('Запрошенных вакансий найти не удалось')
     else:
         user_answer = input(
             'Интересующие Вас вакансии собраны, хотите просмотреть их все или сформировать топ? (введите "ВСЕ" или '
             '"ТОП")\n').lower()
         if user_answer == "ВСЕ".lower():
-            obj_output = Output_user(100)
-            obj_output.output_hh()
+            obj_output = Output_user('hh')
+            obj_output.output()
         elif user_answer == "ТОП".lower():
-            obj_output = Output_user(input('Введите количество ТОП\n'))
-            obj_output.output_hh()
+            obj_output = Output_user('hh', input('Введите количество ТОП\n'))
+            obj_output.output()
         else:
             print('Нет такого варианта ответа, попробуйте сначала')
+
 elif platforms_input == 'SJ'.lower():
-    api = SuperJobAPI(user_vacancy)
-    obj_user_vac = SJ_user_vacancies(api)
-    obj_user_vac.save_user_vacancy()
-    if not open_file_vacancies_sj():
+    if not open_file_vacancies():
         print('Запрошенных вакансий найти не удалось')
     else:
         user_answer = input(
             'Интересующие Вас вакансии собраны, хотите просмотреть их все или сформировать топ? (введите "ВСЕ" или '
             '"ТОП")\n').lower()
         if user_answer == "ВСЕ".lower():
-            obj_output = Output_user(100)
-            obj_output.output_sj()
+            obj_output = Output_user('sj')
+            obj_output.output()
         elif user_answer == "ТОП".lower():
-            obj_output = Output_user(input('Введите количество ТОП\n'))
-            obj_output.output_sj()
+            obj_output = Output_user('sj', input('Введите количество ТОП\n'))
+            obj_output.output()
         else:
             print('Нет такого варианта ответа, попробуйте сначала')
+
 elif platforms_input == 'HH, SJ'.lower() or 'ВСЕ'.lower():
-    api_hh = HeadHunterAPI(user_vacancy)
-    api_sj = SuperJobAPI(user_vacancy)
-    obj_user_vac_hh = HH_user_vacancies(api_hh)
-    obj_user_vac_sj = SJ_user_vacancies(api_sj)
-    obj_user_vac_hh.save_user_vacancy()
-    obj_user_vac_sj.save_user_vacancy()
-    if not open_file_vacancies_hh():
+    if not open_file_vacancies():
         print('Запрошенных вакансий найти не удалось')
     else:
         user_answer = input(
             'Интересующие Вас вакансии собраны, хотите просмотреть их все или сформировать топ? (введите "ВСЕ" или '
             '"ТОП")\n').lower()
         if user_answer == "ВСЕ".lower():
-            obj_output = Output_user(100)
-            obj_output.output_hh()
-            obj_output.output_sj()
+            obj_output_hh = Output_user('hh')
+            obj_output_sj = Output_user('sj')
+            obj_output_hh.output()
+            obj_output_sj.output()
         elif user_answer == "ТОП".lower():
-            obj_output = Output_user(input('Введите количество ТОП\n'))
-            obj_output.output_hh()
-            obj_output.output_sj()
+            num_top = input('Введите количество ТОП\n')
+            obj_output_hh = Output_user('hh', num_top)
+            obj_output_sj = Output_user('sj', num_top)
+            print(f'ТОП {num_top} площадки HH.ru:')
+            obj_output_hh.output()
+            print(f'ТОП {num_top} площадки SuperJob.ru:')
+            obj_output_sj.output()
         else:
             print('Нет такого варианта ответа, попробуйте сначала')
--- src/selection_of_vacancies.py
+++ src/selection_of_vacancies.py
@@ -67,64 +67,42 @@ class SJ_vacancy(Vacancy):
         return dict_vacancies
 
 
-class User_vacancies(ABC):
+class User_vacancies:
     """ Сохраняет содержимое объектов отобранных вакансий в файл """
+    vacancies = {}
 
-    def __init__(self, api_plat):
-        self.api_plat = api_plat
-
-    @abstractmethod
-    def save_user_vacancy(self):
-        pass
-
-
-class HH_user_vacancies(User_vacancies):
+    def __init__(self, api_hh, api_sj):
+        self.api_hh = api_hh
+        self.api_sj = api_sj
 
     def save_user_vacancy(self):
-        json_obj = json.loads(self.api_plat.get_vacancies())
-        vacancy = []
-        for i in json_obj['items']:
-            obj_vacancy = HH_vacancy(i)
-            data = obj_vacancy.attribute_vacancy()
-            vacancy.append(data)
-        recording_hh(vacancy)
-
-
-class SJ_user_vacancies(User_vacancies):
-
-    def save_user_vacancy(self):
-        json_obj = json.loads(self.api_plat.get_vacancies())
-        vacancy = []
-        for i in json_obj['objects']:
-            obj_vacancy = SJ_vacancy(i)
-            data = obj_vacancy.attribute_vacancy()
-            vacancy.append(data)
-        recording_sj(vacancy)
+        json_obj_hh = json.loads(self.api_hh.get_vacancies())
+        json_obj_sj = json.loads(self.api_sj.get_vacancies())
+        list_data_hh = []
+        list_data_sj = []
+        for vac in json_obj_hh['items']:
+            obj_vacancy = HH_vacancy(vac)
+            data_hh = obj_vacancy.attribute_vacancy()
+            list_data_hh.append(data_hh)
+        for vac in json_obj_sj['objects']:
+            obj_vacancy = SJ_vacancy(vac)
+            data_sj = obj_vacancy.attribute_vacancy()
+            list_data_sj.append(data_sj)
+        User_vacancies.vacancies['hh'] = list_data_hh
+        User_vacancies.vacancies['sj'] = list_data_sj
 
 
 class Output_user:
     """ Преобразует вывод информации по вакансиям пользователю """
 
-    def __init__(self, top):
-        self.vacancies_sj = None
-        self.vacancies_hh = None
+    def __init__(self, landing, top=100):
+        self.vacancies = None
         self.top = top
+        self.landing = landing
 
-    def output_hh(self):
-        self.vacancies_hh = open_file_vacancies_hh()
-        top_vacancies = sorted(self.vacancies_hh, key=lambda d: d['salary'], reverse=True)
-        for i in top_vacancies[0:int(self.top)]:
-            print(f"Наименование вакансии - {i['name']}")
-            print(f"Заработная плата - {i['salary']}, уточнить по ссылке ниже")
-            print(f"Описание - {i['specification']}")
-            print(f"Ссылка - {i['url']}")
-            print()
-            print("******************************")
-            print()
-
-    def output_sj(self):
-        self.vacancies_sj = open_file_vacancies_sj()
-        top_vacancies = sorted(self.vacancies_sj, key=lambda d: d['salary'], reverse=True)
+    def output(self):
+        self.vacancies = open_file_vacancies()[self.landing]
+        top_vacancies = sorted(self.vacancies, key=lambda d: d['salary'], reverse=True)
         for i in top_vacancies[0:int(self.top)]:
             print(f"Наименование вакансии - {i['name']}")
             if i['salary'] == 0:
@@ -138,25 +116,13 @@ class Output_user:
             print()
 
 
-def recording_hh(data):  # записывает данные в файл vacancies_hh.json
-    f = open('../files/vacancies_hh.json', mode='w', encoding='utf8')
+def recording(data):  # записывает данные в файл vacancies_hh.json
+    f = open('../files/vacancies.json', mode='w', encoding='utf8')
     f.write(json.dumps(data, ensure_ascii=False))
     f.close()
 
 
-def recording_sj(data):  # записывает данные в файл vacancies_sj.json
-    f = open('../files/vacancies_sj.json', mode='w', encoding='utf8')
-    f.write(json.dumps(data, ensure_ascii=False))
-    f.close()
-
-
-def open_file_vacancies_hh():  # возвращает данные из файла vacancies_hh.json
-    with open('../files/vacancies_hh.json', 'r', encoding='utf8') as file:
-        f = json.load(file)
-        return f
-
-
-def open_file_vacancies_sj():  # возвращает данные из файла vacancies_sj.json
-    with open('../files/vacancies_sj.json', 'r', encoding='utf8') as file:
+def open_file_vacancies():  # возвращает данные из файла vacancies_hh.json
+    with open('../files/vacancies.json', 'r', encoding='utf8') as file:
         f = json.load(file)
         return f
